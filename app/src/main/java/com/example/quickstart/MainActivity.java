515f40a8944970f4ac30dd7a84a6aca0a599ef40
vikukzs <vikuk.zs@gmail.com>
2017-10-09 14:20:16 +0200

    remove commented part

--- app/src/main/java/com/example/quickstart/MainActivity.java
+++ app/src/main/java/com/example/quickstart/MainActivity.java
@@ -249,50 +249,6 @@ public class MainActivity extends Activity
         // Do nothing.
     }
 
-//    /**
-//     * Check that Google Play services APK is installed and up to date.
-//     * @return true if Google Play Services is available and up to
-//     *     date on this device; false otherwise.
-//     */
-//    private boolean isGooglePlayServicesAvailable() {
-//        GoogleApiAvailability apiAvailability =
-//                GoogleApiAvailability.getInstance();
-//        final int connectionStatusCode =
-//                apiAvailability.isGooglePlayServicesAvailable(this);
-//        return connectionStatusCode == ConnectionResult.SUCCESS;
-//    }
-//
-//    /**
-//     * Attempt to resolve a missing, out-of-date, invalid or disabled Google
-//     * Play Services installation via a user dialog, if possible.
-//     */
-//    private void acquireGooglePlayServices() {
-//        GoogleApiAvailability apiAvailability =
-//                GoogleApiAvailability.getInstance();
-//        final int connectionStatusCode =
-//                apiAvailability.isGooglePlayServicesAvailable(this);
-//        if (apiAvailability.isUserResolvableError(connectionStatusCode)) {
-//            showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode);
-//        }
-//    }
-//
-//
-//    /**
-//     * Display an error dialog showing that Google Play Services is missing
-//     * or out of date.
-//     * @param connectionStatusCode code describing the presence (or lack of)
-//     *     Google Play Services on this device.
-//     */
-//    void showGooglePlayServicesAvailabilityErrorDialog(
-//            final int connectionStatusCode) {
-//        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
-//        Dialog dialog = apiAvailability.getErrorDialog(
-//                MainActivity.this,
-//                connectionStatusCode,
-//                REQUEST_GOOGLE_PLAY_SERVICES);
-//        dialog.show();
-//    }
-
     /**
      * An asynchronous task that handles the Google Calendar API call.
      * Placing the API calls in their own task ensures the UI stays responsive.
